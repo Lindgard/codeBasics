0e612a9e24c1c6e9bb17033ef9b35cff97254c05
ALP <a.b.lindgard@gmail.com>
2025-12-23 09:55:17 +0100

    first commit, reading json file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text.Json;
+
+var jsonString = File.ReadAllText("todos.json");
+Console.WriteLine(jsonString);
+
+class Todo
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public bool Completed { get; set; }
+}
