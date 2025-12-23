e9c2e7805fddaefb788071e64440b5392205df2e
ALP <a.b.lindgard@gmail.com>
2025-12-23 10:30:34 +0100

    examples for how to use a json file and ways to use it

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
 ﻿using System.IO;
 using System.Text.Json;
 
+//* hente inn json til programmet fra json fil
 var jsonString = File.ReadAllText("todos.json");
-Console.WriteLine(jsonString);
 
+//* 
+var todos = JsonSerializer.Deserialize<List<Todo>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+//* null guard
+if (todos == null)
+{
+    throw new Exception("could not parse JSON");
+}
+
+var completedTask = todos.Where(todo => todo.Completed == true);
+var unfinishedTask = todos.Where(todo => todo.Completed == false);
+
+var sorted = todos.OrderBy(todo => todo.Completed);
+
+foreach (var todo in sorted)
+{
+    Console.WriteLine(todo.UserId);
+    Console.WriteLine(todo.Id);
+    Console.WriteLine(todo.Title);
+    Console.WriteLine(todo.Completed);
+}
 class Todo
 {
     public int UserId { get; set; }
