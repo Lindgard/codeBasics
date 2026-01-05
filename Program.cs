d41763f2fde2e6b0c9f951d509786a471316f5e0
ALP <a.b.lindgard@gmail.com>
2026-01-05 09:02:59 +0100

    first day back from holidays

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 //* hente inn json til programmet fra json fil
 var jsonString = File.ReadAllText("todos.json");
 
-//* 
+//* JSON parses til en liste med Todos, case-sensitive matching trengs fordi JSON er camelCase
 var todos = JsonSerializer.Deserialize<List<Todo>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 //* null guard
 if (todos == null)
@@ -12,11 +12,16 @@ if (todos == null)
     throw new Exception("could not parse JSON");
 }
 
+//* Where filtrerer todos basert på completion state. LINQ-operator, Select er en
+//* Annen LINQ-operator som er veldig vanlig å bruke til å transformere element.
 var completedTask = todos.Where(todo => todo.Completed == true);
 var unfinishedTask = todos.Where(todo => todo.Completed == false);
 
+//* OrderBy er fra LINQ og lar oss sortere basert på det vi passer inn med arrow-function i parantesen
 var sorted = todos.OrderBy(todo => todo.Completed);
 
+//* Loop gjennom hvert objekt basert på sorted over og print ut med Console.WriteLine.
+//* Bruker da keys fra klassen under til å hente riktig informasjon fra filen
 foreach (var todo in sorted)
 {
     Console.WriteLine(todo.UserId);
@@ -24,6 +29,8 @@ foreach (var todo in sorted)
     Console.WriteLine(todo.Title);
     Console.WriteLine(todo.Completed);
 }
+
+//* Klasse som representerer todo items fra JSON payload(data fra filen)
 class Todo
 {
     public int UserId { get; set; }
